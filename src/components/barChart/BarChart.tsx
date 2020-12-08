ecda5b58ef018ab1adb1a717de90ced3b167544a
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-12-08 18:02:49 +0200

    Follow demo example

--- src/components/barChart/BarChart.tsx
+++ src/components/barChart/BarChart.tsx
@@ -1,17 +1,36 @@
 import React from 'react'
+import { VictoryAxis, VictoryBar, VictoryChart, VictoryTheme } from 'victory'
+
 import { Props } from './types'
 
 const BarChart: React.FC<{ props: Props }> = ({ props }) => {
   const { data } = props
 
-  const printData = () => {
-    data.forEach(d => console.log('data row', d))
+  const createTickValues = (): number[] => {
+    const numbersFromZero = [...Array(data.length).keys()]
+    return numbersFromZero.map(n => n + 1)
   }
   
   return (
     <div>
-      This is a bar chart
-      <button onClick={printData}>print</button>
+      <VictoryChart
+        domainPadding={20}
+        theme={VictoryTheme.material}
+      >
+        <VictoryAxis
+          tickFormat={[ 'Quarter 1', 'Quarter 2', 'Quarter 3', 'Quarter 4' ]}
+          tickValues={createTickValues()}
+        />
+        <VictoryAxis
+          dependentAxis
+          tickFormat={x => `$${x / 1000}k`}
+        />
+        <VictoryBar
+          data={data}
+          x='quarter'
+          y='earnings'
+        />
+      </VictoryChart>
     </div>
   )
 }
