85d3d897af7fc207c59a2e9a4ae85f4635addf2c
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-12-12 14:54:37 +0200

    Got fill style working

--- src/components/scatter/ScatterChart.tsx
+++ src/components/scatter/ScatterChart.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { VictoryScatter } from 'victory'
 
 import ZoomContainer from '../../containers/ZoomContainer'
-import { TestData } from './data'
-import { style } from './style'
-import { Domain } from '../../containers/types'
+import { Coordinate } from '../../data/dataTypes'
+// import { createStyle } from './style'
+import { Domain } from '../../containers/victoryTypes'
 
 interface Props {
-  data: TestData[]
+  data: Coordinate[]
 }
 
 const domain: Domain = {
@@ -15,16 +15,26 @@ const domain: Domain = {
   y: [0, 100]
 }
 
-const ScatterChart: React.FC<Props> = ({ data }) => (
-  <ZoomContainer domain={domain}>
-    <VictoryScatter
-      data={data}
-      size={7}
-      style={style}
-      x='x'
-      y='y'
-    />        
-  </ZoomContainer>
-)
+const ScatterChart: React.FC<Props> = ({ data }) => {
+
+  const mapFillStyle = (coordinate: Coordinate): string =>
+    coordinate.y % 2 === 0 ? 'black' : 'tomato'
+  
+  return ( 
+    <ZoomContainer domain={domain}>
+      <VictoryScatter
+        data={data}
+        size={7}
+        style={{
+          data: {
+            fill: data => mapFillStyle(data.datum)
+          }
+        }}
+        x='x'
+        y='y'
+      />        
+    </ZoomContainer>
+  )
+}
 
 export default ScatterChart
--- src/components/scatter/data.ts
+++ src/components/scatter/data.ts
@@ -2,8 +2,8 @@ import { Coordinate } from '../../data/dataTypes'
 
 export const scatterData: Coordinate[] = [
   { x: 10, y: 20 },
-  { x: 20, y: 30 },
+  { x: 20, y: 31 },
   { x: 30, y: 50 },
-  { x: 40, y: 40 },
+  { x: 40, y: 47 },
   { x: 50, y: 70 },
 ]
