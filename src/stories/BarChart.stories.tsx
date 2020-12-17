13df0d85fef9bbf1158655423f9382bbb53545e2
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-12-17 19:19:23 +0200

    Install addon-console for Storybook

--- src/stories/BarChart.stories.tsx
+++ src/stories/BarChart.stories.tsx
@@ -3,13 +3,14 @@ import { Story, Meta } from '@storybook/react'
 
 import BarChart, { Props } from '../components/barChart/BarChart'
 import { data } from '../data'
+import { Datum } from '../types/dataTypes'
 
 const Template: Story<Props> = (args) => <BarChart { ...args } />
 
 export const PrimaryBarChart = Template.bind({})
 PrimaryBarChart.args = {
   data,
-  onClick: () => console.log('data', data)
+  onClick: ({ x, y, isSelected }: Datum) => console.log('x', x, 'y', y, 'selected', isSelected)
 }
 
 export default {
