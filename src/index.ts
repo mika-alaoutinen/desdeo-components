ddc19a5971990d568a053fcf82a508936e41113e
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-12-18 17:08:34 +0200

    Delete unnecessary files after introducing Storybook

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+// Export the components for publishing
+export { default as BarChartComponent } from './components/barChart/BarChart'
+export { default as StackedBarChartComponent } from './components/barChart/StackedBarChart'
+export { default as ScatterChartComponent } from './components/scatter/ScatterChart'
