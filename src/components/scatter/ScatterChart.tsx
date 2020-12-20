c2fda3dc47c08e6ae673baf392981ed18b155109
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-12-20 10:40:12 +0200

    Add onClick event handling to scatter chart

--- src/components/scatter/ScatterChart.tsx
+++ src/components/scatter/ScatterChart.tsx
@@ -6,26 +6,47 @@ import { Domain } from '../../types/containerTypes'
 import { DataProps } from '../../types/dataTypes'
 import { mapFillStyle, mapOpacityStyle } from '../../styles/style'
 
+import { BarPropsExt } from '../../types/extendedTypes'
+import { updateSelected } from '../../events/BarChartEvents'
+
 const domain: Domain = {
   x: [0, 100],
   y: [0, 100]
 }
 
-const ScatterChart: React.FC<DataProps> = ({ data }) => ( 
-  <ZoomContainer domain={domain}>
-    <VictoryScatter
-      data={data}
-      size={7}
-      style={{
-        data: {
-          fill: data => mapFillStyle(data.datum),
-          opacity: data => mapOpacityStyle(data.datum)
-        }
-      }}
-      x='x'
-      y='y'
-    />        
-  </ZoomContainer>
-)
+const ScatterChart: React.FC<DataProps> = ({ data, onClick }) => {
+  const clickHandler = (props: BarPropsExt): BarPropsExt => {
+    const editedProps = updateSelected(props)
+    if (onClick) {
+      onClick(editedProps.datum)
+    }
+    return editedProps
+  }
+  
+  return (
+    <ZoomContainer domain={domain}>
+      <VictoryScatter
+        data={data}
+        events={[
+          {
+            target: 'data',
+            eventHandlers: {
+              onClick: () => [{
+                mutation: (props: BarPropsExt) => clickHandler(props)
+              }]
+            }
+          }
+        ]}
+        size={7}
+        style={{
+          data: {
+            fill: data => mapFillStyle(data.datum),
+            opacity: data => mapOpacityStyle(data.datum)
+          }
+        }}
+      />        
+    </ZoomContainer>
+  )
+}
 
 export default ScatterChart
