f95693e3fe7e5afeb51fa3da27b1cd95219e90bf
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-12-28 16:27:45 +0200

    Fix SelectionContainer by adding a separate npm dependency

--- src/types/containerTypes.ts
+++ src/types/containerTypes.ts
@@ -1,16 +1,5 @@
-import { Datum } from './dataTypes'
-
 // --- Interfaces for Victory's container classes ---
 export interface Domain {
   x: [ number, number ],
   y: [ number, number ]
 }
-
-export interface SelectedDatum {
-  childName: string,
-  data: Datum[],
-  eventKey: number[]
-}
-
-// --- Types for Victory's container classes ---
-export type SelectedData = [ SelectedDatum ] // The points argument in onSelection is an array with one item.
