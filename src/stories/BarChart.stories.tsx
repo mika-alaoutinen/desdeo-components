fe0c2536d2e9ac762d599e257daf436700a28cf0
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-12-29 15:05:04 +0200

    Delete story for Stacked Bar Chart and add it to Bar Chart stories

--- src/stories/BarChart.stories.tsx
+++ src/stories/BarChart.stories.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { Meta } from '@storybook/react'
 
 import BarChart from '../components/barChart/BarChart'
+import StackedBarChart from '../components/barChart/StackedBarChart'
 import { TestEventHandler, useReactHandler, useReduxHandler } from './storyUtils'
+import { barData } from './testdata'
 
 export const BarChartWithUseState = (): JSX.Element => {
   const { data, eventHandler }: TestEventHandler = useReactHandler()
@@ -25,6 +27,10 @@ export const BarChartWithRedux = (): JSX.Element => {
     />
   )
 }
+
+export const UninteractiveStackedBarChart = (): JSX.Element => (
+  <StackedBarChart data={barData} />
+)
 
 export default {
   title: 'DESDEO/BarChart',
