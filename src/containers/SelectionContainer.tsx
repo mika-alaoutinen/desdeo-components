c3f62937ff07cf00acf2a8bc2ad4a6076b1ee393
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-12-30 13:02:19 +0200

    Refactor SelectionContainer to use OnSelectChart props

--- src/containers/SelectionContainer.tsx
+++ src/containers/SelectionContainer.tsx
@@ -1,31 +1,18 @@
 import React from 'react'
-import { PointProps } from 'victory'
 // The selection container export in victory is broken.
 // Let's import it from the separate victory-selection-container npm package instead...
 import { VictorySelectionContainer } from 'victory-selection-container'
 
 import ChartContainer from './ChartContainer'
 import { onSelectHandler, selectionClearedHandler } from '../events/onSelect'
-import { Datum } from '../types/dataTypes'
-import { OnSelectHandler } from '../types/eventTypes'
+import { OnSelectChart } from '../types/chartTypes'
 
-interface Props {
-  data: Datum[],
-  onSelect: OnSelectHandler
-}
+const SelectionContainer: React.FC<OnSelectChart> = ({ data, onSelect, ...props }) => {
 
-const SelectionContainer: React.FC<Props> = ({ data, onSelect, ...props }) => {
-
-  const selection = (points: PointProps[]): void =>
-    onSelectHandler(points[0].data, data, onSelect)
-  
-  const selectionCleared = (): void =>
-    selectionClearedHandler(data, onSelect)
-  
   const selectionContainer = (): JSX.Element =>
     <VictorySelectionContainer
-      onSelection={selection}
-      onSelectionCleared={selectionCleared}
+      onSelection={points => onSelectHandler(points[0].data, data, onSelect)}
+      onSelectionCleared={() => selectionClearedHandler(data, onSelect)}
     />
   
   return (
