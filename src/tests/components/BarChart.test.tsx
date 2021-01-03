039c524af5410089ba1fa3c4140e51c6a22b400a
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-01-03 19:14:49 +0200

    Move getPath to testUtils

--- src/tests/components/BarChart.test.tsx
+++ src/tests/components/BarChart.test.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { fireEvent, render, screen } from '@testing-library/react'
 
 import BarChart from '../../components/barChart/BarChart'
-import { createOnClickHandler } from '../testUtils'
+import { createOnClickHandler, getPaths } from '../testUtils'
 import { testdata } from '../testdata'
 
+// Constants
 const handler = createOnClickHandler('USE_STATE')
+const component = <BarChart data={testdata} onClick={handler} />
 
 describe('BarChart is rendered correctly', () => {
   it('is truthy', () => {
@@ -13,12 +15,12 @@ describe('BarChart is rendered correctly', () => {
   })
 
   it('chart is rendered', () => {
-    const chart = render(<BarChart data={testdata} onClick={handler} />)
+    const chart = render(component)
     expect(chart).toBeTruthy()
   })
 
   it('Victory container is rendered', () => {
-    const { container } = render(<BarChart data={testdata} onClick={handler} />)
+    const { container } = render(component)
     const victoryContainers = container.getElementsByClassName('VictoryContainer')
     expect(victoryContainers).toHaveLength(1)
     expect(victoryContainers[0]).toBeTruthy()
@@ -27,27 +29,22 @@ describe('BarChart is rendered correctly', () => {
 
 describe('Data is displayed correctly', () => {
   it('has data labels A-E', () => {
-    render(<BarChart data={testdata} onClick={handler} />)
+    render(component)
     const expectedLabels = [ 'A', 'B', 'C', 'D', 'E' ]
     expectedLabels.forEach(label =>
       expect(screen.getAllByText(label)).toHaveLength(1))
   })
 
   it('has five path elements representing bars', () => {
-    expect(getPaths()).toHaveLength(5)
+    const paths = getPaths(component)
+    expect(paths).toHaveLength(5)
   })
 })
 
 describe('Bars should be clickable', () => {
   it('registers a click event', () => {
-    const paths = getPaths()
+    const paths = getPaths(component)
     fireEvent.click(paths[0])
     expect(handler.fn).toHaveBeenCalled()
   })
 })
-
-// Utility functions
-const getPaths = (): NodeListOf<SVGPathElement> => {
-  const { container } = render(<BarChart data={testdata} onClick={handler} />)
-  return container.querySelectorAll('path')
-}
--- src/tests/testUtils.ts
+++ src/tests/testUtils.ts
@@ -1,3 +1,5 @@
+import { render } from '@testing-library/react'
+
 import { OnClickHandler, OnSelectHandler } from '../types/eventTypes'
 
 type handler = 'REDUX'|'USE_STATE'|undefined
@@ -11,3 +13,9 @@ export const createOnSelectHandler = (type: handler): OnSelectHandler => ({
   type,
   fn: jest.fn()
 })
+
+// The datum in a Victory chart are rendered as SVG path elements
+export const getPaths = (component: React.ReactElement): NodeListOf<SVGPathElement> => {
+  const { container } = render(component)
+  return container.querySelectorAll('path')
+}
