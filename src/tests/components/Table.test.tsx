b8e85a2475ace942cb50fd141f060d657dd02021
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-01-03 22:20:54 +0200

    Add tests for Table

---
+++ src/tests/components/Table.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { fireEvent, render, screen } from '@testing-library/react'
+
+import Table from '../../components/table/Table'
+import { renderComponent, renderDataLabels } from './componentTests'
+import { createOnClickHandler } from '../testUtils'
+import { testdata } from '../testdata'
+
+// Constants
+const handler = createOnClickHandler('USE_STATE')
+const component = <Table data={testdata} onClick={handler} />
+
+describe('Table is rendered correctly', () => {
+  it('is truthy', () => {
+    expect(Table).toBeTruthy()
+  })
+
+  it('component is rendered', () => {
+    renderComponent(component)
+  })
+})
+
+describe('Data is displayed correctly', () => {
+  it('has data labels A-E', () => {
+    renderDataLabels(component)
+  })
+})
+
+describe('Rows should be clickable', () => {
+  it('registers a click event', () => {
+    render(component)
+    fireEvent.click(screen.getByText('A'))
+    expect(handler.fn).toHaveBeenCalled()
+  })
+})
