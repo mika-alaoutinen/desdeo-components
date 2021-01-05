14a67bc63bcfdfdf63d4c23083264edfc5136e1c
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-01-05 11:19:16 +0200

    Refactor TableRow tests and add test case for onClick

--- src/tests/components/TableRow.test.tsx
+++ src/tests/components/TableRow.test.tsx
@@ -7,39 +7,51 @@ import TableRow from '../../components/table/TableRow'
 import { ROW_SELECTED_COLOR } from '../../components/table/tableStyles'
 import { Datum } from '../../types/dataTypes'
 
+// Constants
+const handler = jest.fn()
+
 describe('TableRow is rendered correctly', () => {
   it('is truthy', () => {
     expect(TableRow).toBeTruthy()
   })
 
   it('component is rendered', () => {
-    expect(renderComponent()).toBeTruthy()
+    expect(renderComponent(createDatum())).toBeTruthy()
   })
 })
 
 describe('Background color of a row changes on mouse hover', () => {
   it('background is white by default', () => {
-    const container = renderComponent()
+    const container = renderComponent(createDatum())
     const row = container.querySelector('tr')
     expect(row).toHaveStyle({ background: 'white' })
   })
 
   it('background is #ffdfda when row is selected', () => {
-    const container = renderComponent(true)
+    const container = renderComponent(createDatum(true))
     const row = container.querySelector('tr')
     expect(row).toHaveStyle({ background: ROW_SELECTED_COLOR })
   })
 
   it('background is whitesmoke on mouse hover', () => {
-    const container = renderComponent()
+    const container = renderComponent(createDatum())
     const row = container.querySelector('tr')
     fireEvent.mouseEnter(row)
     expect(row).toHaveStyle({ background: 'whitesmoke' })
   })
 })
 
-const renderComponent = (isSelected?: boolean): Element => {
-  const component = createComponent(isSelected)
+describe('Clicking on a table row calls onClick function', () => {
+  it('onClick is called', () => {
+    const container = renderComponent(createDatum())
+    const row = container.querySelector('tr')
+    fireEvent.click(row)
+    expect(handler).toHaveBeenCalled()
+  })
+})
+
+const renderComponent = (datum: Datum): Element => {
+  const component = createComponent(datum)
   const tbody = document.createElement('tbody')
   const { container } = render(component, {
     container: document.body.appendChild(tbody)
@@ -47,14 +59,13 @@ const renderComponent = (isSelected?: boolean): Element => {
   return container
 }
 
-const createComponent = (isSelected?: boolean): JSX.Element => {
-  const datum: Datum = {
-    id: '123',
-    label: 'A',
-    isSelected,
-    x: 1,
-    y: 1
-  }
+const createComponent = (datum: Datum): JSX.Element =>
+  <TableRow datum={datum} onClick={handler} />
 
-  return <TableRow datum={datum} onClick={jest.fn()} />
-}
+const createDatum = (isSelected?: boolean): Datum => ({
+  id: '123',
+  label: 'A',
+  isSelected,
+  x: 1,
+  y: 1
+})
