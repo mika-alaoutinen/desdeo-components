f084528bdde007970a55179949b5ea64eefffadb
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-01-16 15:27:34 +0200

    Update story

--- src/stories/ParallelAxes.stories.tsx
+++ src/stories/ParallelAxes.stories.tsx
@@ -1,16 +1,8 @@
 import React from 'react'
 import { Meta } from '@storybook/react'
 
-// import ParallelAxes from '../components/parallelAxes/ParallelAxes'
-import ParallelAxesExample from '../components/parallelAxes/ParallelAxesExample'
 import ParallelAxes from '../components/parallelAxes/ParallelAxes'
 import { parallelAxesData } from '../tests/testdata'
-
-export const ParallelAxesDemo = (): JSX.Element => (
-  <ParallelAxesExample
-    data={parallelAxesData}
-  />
-)
 
 export const ParallelAxesComponent = (): JSX.Element => (
   <ParallelAxes
@@ -20,5 +12,5 @@ export const ParallelAxesComponent = (): JSX.Element => (
 
 export default {
   title: 'DESDEO/ParallelAxes',
-  component: ParallelAxesExample
+  component: ParallelAxes
 } as Meta
