2b6469a9771c624856df5263897c0f8402563ec4
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-01-16 16:23:03 +0200

    Move addNewFilters from component to utils

--- src/components/parallelAxes/ParallelAxes.tsx
+++ src/components/parallelAxes/ParallelAxes.tsx
@@ -4,7 +4,7 @@ import { VictoryAxis, VictoryBrushLine, VictoryChart, VictoryLine } from 'victor
 import AttributeLabels from './AttributeLabels'
 import { getAttributeNames, getMaxAttributeValues } from './dataParser'
 import { layout } from './layout'
-import { calculateAxisOffset, getActiveDatasets } from './utils'
+import { addNewFilters, calculateAxisOffset, getActiveDatasets } from './utils'
 import { normalizeData } from './dataTransformations'
 import { Filter, ParallelAxesData } from '../../types/dataTypes'
 
@@ -31,30 +31,8 @@ const ParallelAxes: React.FC<Props> = ({ data }) => {
     // The domain numbers emitted by VictoryBrushLine are in the wrong order of [max, min].
     // Flip the numbers around so that they make sense as a range.
     const domain: [number, number] = [domainTuple[1], domainTuple[0]]
-    setFilters(addNewFilters(domain, name))
+    setFilters(addNewFilters(filters, domain, name))
     setActiveDataSets(getActiveDatasets(datasets, filters))
-  }
-
-  const addNewFilters = (domain: [number, number], name: string): Filter[] => {
-    const extent = domain && Math.abs(domain[1] - domain[0])
-    const minVal = 1 / Number.MAX_SAFE_INTEGER
-    const maxDomainValue = 10
-
-    // If there is no existing filter for an attribute, create one
-    if (!filters.some(filter => filter.attribute === name)) {
-      return filters.concat({
-        attribute: name,
-        range: extent <= minVal ? [minVal, maxDomainValue] : domain
-      })
-    }
-
-    return filters.map(filter => filter.attribute === name
-      ? {
-        ...filter,
-        range: extent <= minVal ? [minVal, maxDomainValue] : domain
-      }
-      : filter
-    )
   }
 
   // Draw chart elements. For whatever reason putting the rendering code
