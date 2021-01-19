8f97b0e490c714ceebdc3cf4ea2c2cdb48723fb7
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-01-19 15:44:47 +0200

    Export GroupedBarChart

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // Export the components for publishing
 export { default as BarChartComponent } from './components/bar/BarChart'
 export { default as ParallelAxesComponent } from './components/parallelAxes/ParallelAxes'
+export { default as GroupedBarChartComponent } from './components/bar/GroupedBarChart'
 export { default as StackedBarChartComponent } from './components/bar/StackedBarChart'
 export { default as ScatterChartComponent } from './components/scatter/ScatterChart'
 export { default as ScatterSelectionComponent } from './components/scatter/ScatterSelection'
