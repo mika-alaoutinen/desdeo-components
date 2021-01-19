223c680946910d70ef813763df2788c9509dc1cb
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-01-19 19:11:01 +0200

    Edit exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 // Export the components for publishing
 export { default as BarChartComponent } from './components/bar/BarChart'
-export { default as ParallelAxesComponent } from './components/parallelAxes/ParallelAxes'
 export { default as GroupedBarChartComponent } from './components/bar/GroupedBarChart'
 export { default as StackedBarChartComponent } from './components/bar/StackedBarChart'
+
+export { default as ParallelAxesComponent } from './components/parallelAxes/ParallelAxes'
+
 export { default as ScatterChartComponent } from './components/scatter/ScatterChart'
 export { default as ScatterSelectionComponent } from './components/scatter/ScatterSelection'
+
 export { default as TableComponent } from './components/table/Table'
