569e2925216b1adcf395736c5fa122662514f8c0
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-01-19 17:57:29 +0200

    Add layoutTypes and a layout file for bar charts

---
+++ src/types/layoutTypes.ts
@@ -0,0 +1,12 @@
+export interface Layout {
+  height: number,
+  width: number,
+  padding: Padding
+}
+
+export interface Padding {
+  top: number,
+  left: number,
+  right: number,
+  bottom: number
+}
