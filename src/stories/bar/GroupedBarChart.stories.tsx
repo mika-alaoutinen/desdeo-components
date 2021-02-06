904f138688697d3ef40eabf1113129c1019e0770
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-02-06 19:35:45 +0200

    Add more stories for grouped bar chart

--- src/stories/bar/GroupedBarChart.stories.tsx
+++ src/stories/bar/GroupedBarChart.stories.tsx
@@ -4,31 +4,41 @@ import { Meta } from '@storybook/react'
 import GroupedBarChart from 'components/bar/GroupedBarChart'
 import { groupedByAlternatives, groupedByCriteria } from 'tests/testdata'
 import { Coordinate } from 'types/dataTypes'
+import { range } from 'utils/utilFunctions'
 
-// Click handler
 const clickHandler = (coordinate: Coordinate) => console.log('coordinate', coordinate)
 
-export const HorizontalGroupedBarChart = (): JSX.Element => {
-  const range = [...Array(10).keys()].map(i => i + 1)
-  const labels = range.map(n => `Alternative\n${n}`)
+// Grouping data by alternative solutions
+const alternativesComponent = (horizontal: boolean): JSX.Element => (
+  <GroupedBarChart
+    datasets={groupedByAlternatives}
+    labels={range(10).map(n => `Alternative\n${n}`)}
+    onClick={clickHandler}
+    horizontal={horizontal}
+  />
+)
 
-  return (
-    <GroupedBarChart
-      datasets={groupedByAlternatives}
-      labels={labels}
-      onClick={clickHandler}
-      horizontal={true}
-    />
-  )
-}
+export const GroupedByAlternativesHorizontal = (): JSX.Element =>
+  alternativesComponent(true)
 
-export const VerticalGroupedBarChart = (): JSX.Element => (
+export const GroupedByAlternativesVertical = (): JSX.Element =>
+  alternativesComponent(false)
+
+// Grouping data by criteria
+const criteriaComponent = (horizontal: boolean): JSX.Element => (
   <GroupedBarChart
     datasets={groupedByCriteria}
     labels={[ 'WQ\nFishery', 'WQ\nCity', 'ROI', 'City\nTax', 'Plant\nResources' ]}
     onClick={clickHandler}
+    horizontal={horizontal}
   />
 )
+
+export const GroupedByCriteriaHorizontal = (): JSX.Element =>
+  criteriaComponent(true)
+
+export const GroupedByCriteriaVertical = (): JSX.Element =>
+  criteriaComponent(false)
 
 export default {
   title: 'DESDEO/GroupedBarChart',
