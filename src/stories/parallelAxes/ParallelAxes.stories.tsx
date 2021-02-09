0ea9290b2d9c1614001cfbd5ce7f1556572ed255
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-02-09 19:41:38 +0200

    Fix story

--- src/stories/parallelAxes/ParallelAxes.stories.tsx
+++ src/stories/parallelAxes/ParallelAxes.stories.tsx
@@ -3,10 +3,10 @@ import { Meta } from '@storybook/react'
 
 import ParallelAxes from 'components/parallelAxes/ParallelAxes'
 import ParallelAxesWrapper from 'components/parallelAxesWrapper/ParallelAxesWrapper'
-import { parallelAxesData } from 'tests/testdata'
+import { parallelAxes } from 'tests/testdata'
 
 export const ParallelAxesComponent = (): JSX.Element =>
-  <ParallelAxesWrapper data={parallelAxesData} />
+  <ParallelAxesWrapper data={parallelAxes} />
 
 export default {
   title: 'DESDEO/ParallelAxes',
