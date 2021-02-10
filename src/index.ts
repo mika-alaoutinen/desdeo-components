3e54347a8cb654ea7a1737cb189d90c3d2e59103
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-02-10 16:22:35 +0200

    Export parallel axes wrapper

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export { default as BarChartComponent } from 'components/bar/BarChart'
 export { default as GroupedBarChartComponent } from 'components/bar/GroupedBarChart'
 export { default as StackedBarChartComponent } from 'components/bar/StackedBarChart'
 
-export { default as ParallelAxesComponent } from 'components/parallelAxes/ParallelAxes'
+export { default as ParallelAxesComponent } from 'components/parallelAxesWrapper/ParallelAxesWrapper'
 
 export { default as ScatterChartComponent } from 'components/scatter/ScatterChart'
 export { default as ScatterSelectionComponent } from 'components/scatter/ScatterSelection'
