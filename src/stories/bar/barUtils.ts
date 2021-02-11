49598b495ba293d96f9470d7d81ab524efdff9ab
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-02-11 19:29:47 +0200

    Update stories

--- src/stories/bar/barUtils.ts
+++
@@ -1,9 +0,0 @@
-import { groupedByAlternatives } from '../../tests/testdata'
-import { range } from '../../utils/utils'
-
-const createAlternativesLabels = (): string[] => {
-  const alternatives = groupedByAlternatives[0].data.length
-  return range(alternatives).map(n => `Alternative\n${n}`)
-}
-
-export { createAlternativesLabels }
