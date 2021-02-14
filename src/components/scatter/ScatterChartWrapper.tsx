419c31c5efdf89719534cbba15e70bb72ef3bbbc
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-02-14 20:00:46 +0200

    Add wrapper for scatter chart and update story

---
+++ src/components/scatter/ScatterChartWrapper.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import ScatterChart from './ScatterChart'
+import { createCoordinates } from '../../data/transformations'
+import { OnClickHandler } from '../../types/chartTypes'
+import { DataSetTuple } from '../../types/dataTypes'
+
+export interface Props {
+  data: DataSetTuple,
+  onClick: OnClickHandler
+}
+
+const ScatterChartWrapper: React.FC<Props> = ({ data, onClick }) => {
+  return (
+    <ScatterChart
+      data={createCoordinates(data)}
+      onClick={onClick}
+    />
+  )
+}
+
+export default ScatterChartWrapper
--- src/stories/scatter/ScatterChart.stories.tsx
+++ src/stories/scatter/ScatterChart.stories.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import { Meta } from '@storybook/react'
+import { Meta, Story } from '@storybook/react'
 
 import ScatterChart from '../../components/scatter/ScatterChart'
+import ScatterChartWrapper, { Props } from '../../components/scatter/ScatterChartWrapper'
 import { useOnClickHandler } from '../storyUtils'
+import { datasetTuple } from '../../tests/testdata'
 
 export const ScatterChartComponent = (): JSX.Element => {
   const { data, onClick } = useOnClickHandler()
@@ -15,7 +17,16 @@ export const ScatterChartComponent = (): JSX.Element => {
   )
 }
 
+const Template: Story<Props> = args =>
+  <ScatterChartWrapper {...args} />
+
+export const GroupedBarChart = Template.bind({})
+GroupedBarChart.args = {
+  data: datasetTuple,
+  onClick: coordinate => console.log('clicked', coordinate)
+}
+
 export default {
   title: 'DESDEO/ScatterChart',
-  component: ScatterChart
+  component: ScatterChartWrapper
 } as Meta
