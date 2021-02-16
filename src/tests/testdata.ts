3375b38b8ac73e791ad87faeab96d453a6174f4c
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-02-16 17:15:30 +0200

    Update testdata

--- src/tests/testdata.ts
+++ src/tests/testdata.ts
@@ -5,34 +5,116 @@ import {
 const dataset: DataSet = [
   {
     label: 'WQ Fishery',
-    data: [ 6.042483, 5.758127, 6.287081, 6.134672, 5.610188, 5.231501, 6.34, 6.291364, 5.407513, 6.019503 ]
+    data: [
+      { id: 'wq-fishery-0', isSelected: false, value: 6.042483 },
+      { id: 'wq-fishery-1', isSelected: false, value: 5.758127 },
+      { id: 'wq-fishery-2', isSelected: false, value: 6.287081 },
+      { id: 'wq-fishery-3', isSelected: false, value: 6.134672 },
+      { id: 'wq-fishery-4', isSelected: false, value: 5.610188 },
+      { id: 'wq-fishery-5', isSelected: false, value: 5.231501 },
+      { id: 'wq-fishery-6', isSelected: false, value: 6.34 },
+      { id: 'wq-fishery-7', isSelected: false, value: 6.291364 },
+      { id: 'wq-fishery-8', isSelected: false, value: 5.407513 },
+      { id: 'wq-fishery-9', isSelected: false, value: 6.019503 },
+    ]
   },
+
   {
     label: 'WQ City',
-    data: [ 3.17527, 3.410843, 3.207926, 2.98383, 2.910456, 3.248641, 2.962557, 3.346416, 3.130143, 3.350959 ]
+    data: [
+      { id: 'wq-city-0', isSelected: false, value: 3.17527 },
+      { id: 'wq-city-1', isSelected: false, value: 3.410843 },
+      { id: 'wq-city-2', isSelected: false, value: 3.207926 },
+      { id: 'wq-city-3', isSelected: false, value: 2.98383 },
+      { id: 'wq-city-4', isSelected: false, value: 2.910456 },
+      { id: 'wq-city-5', isSelected: false, value: 3.248641 },
+      { id: 'wq-city-6', isSelected: false, value: 2.962557 },
+      { id: 'wq-city-7', isSelected: false, value: 3.346416 },
+      { id: 'wq-city-8', isSelected: false, value: 3.130143 },
+      { id: 'wq-city-9', isSelected: false, value: 3.350959 },
+    ]
   },
+
   {
     label: 'ROI',
-    data: [ 6.090291, 6.887735, 2.992514, 5.507545, 7.082375, 7.352708, 0.321111, 2.847139, 7.254194, 6.195485 ]
+    data: [
+      { id: 'roi', isSelected: false, value: 6.090291 },
+      { id: 'roi', isSelected: false, value: 6.887735 },
+      { id: 'roi', isSelected: false, value: 2.992514 },
+      { id: 'roi', isSelected: false, value: 5.507545 },
+      { id: 'roi', isSelected: false, value: 7.082375 },
+      { id: 'roi', isSelected: false, value: 7.352708 },
+      { id: 'roi', isSelected: false, value: 0.321111 },
+      { id: 'roi', isSelected: false, value: 2.847139 },
+      { id: 'roi', isSelected: false, value: 7.254194 },
+      { id: 'roi', isSelected: false, value: 6.195485 },
+    ]
   },
+
   {
     label: 'City Tax',
-    data: [ 2.444406, 8.989781, 2.758216, 0.581456, 0.216794, 3.951754, 0.377181, 5.67065, 2.057297, 6.173211 ]
+    data: [
+      { id: 'city-tax', isSelected: false, value: 2.444406 },
+      { id: 'city-tax', isSelected: false, value: 8.989781 },
+      { id: 'city-tax', isSelected: false, value: 2.758216 },
+      { id: 'city-tax', isSelected: false, value: 0.581456 },
+      { id: 'city-tax', isSelected: false, value: 0.216794 },
+      { id: 'city-tax', isSelected: false, value: 3.951754 },
+      { id: 'city-tax', isSelected: false, value: 0.377181 },
+      { id: 'city-tax', isSelected: false, value: 5.67065 },
+      { id: 'city-tax', isSelected: false, value: 2.057297 },
+      { id: 'city-tax', isSelected: false, value: 6.173211 },
+    ]
   },
+
   {
     label: 'Plant Resources',
-    data: [ 0.248895, 0.346752, 0.326688, 0.259547, 0.126336, 0.295807, 0.35, 0.328574, 0.228541, 0.327455 ]
+    data: [
+      { id: 'plant-resources', isSelected: false, value: 0.248895 },
+      { id: 'plant-resources', isSelected: false, value: 0.346752 },
+      { id: 'plant-resources', isSelected: false, value: 0.326688 },
+      { id: 'plant-resources', isSelected: false, value: 0.259547 },
+      { id: 'plant-resources', isSelected: false, value: 0.126336 },
+      { id: 'plant-resources', isSelected: false, value: 0.295807 },
+      { id: 'plant-resources', isSelected: false, value: 0.35 },
+      { id: 'plant-resources', isSelected: false, value: 0.328574 },
+      { id: 'plant-resources', isSelected: false, value: 0.228541 },
+      { id: 'plant-resources', isSelected: false, value: 0.327455 },
+    ]
   }
 ]
 
+// Delete this
 const datasetTuple: DataSetTuple = [
   {
     label: 'WQ Fishery',
-    data: [ 6.042483, 5.758127, 6.287081, 6.134672, 5.610188, 5.231501, 6.34, 6.291364, 5.407513, 6.019503 ]
+    data: [
+      { id: 'wq-fishery-0', isSelected: false, value: 6.042483 },
+      { id: 'wq-fishery-1', isSelected: false, value: 5.758127 },
+      { id: 'wq-fishery-2', isSelected: false, value: 6.287081 },
+      { id: 'wq-fishery-3', isSelected: false, value: 6.134672 },
+      { id: 'wq-fishery-4', isSelected: false, value: 5.610188 },
+      { id: 'wq-fishery-5', isSelected: false, value: 5.231501 },
+      { id: 'wq-fishery-6', isSelected: false, value: 6.34 },
+      { id: 'wq-fishery-7', isSelected: false, value: 6.291364 },
+      { id: 'wq-fishery-8', isSelected: false, value: 5.407513 },
+      { id: 'wq-fishery-9', isSelected: false, value: 6.019503 },
+    ]
   },
   {
     label: 'WQ City',
-    data: [ 3.17527, 3.410843, 3.207926, 2.98383, 2.910456, 3.248641, 2.962557, 3.346416, 3.130143, 3.350959 ]
+    data: [
+      { id: 'wq-city-0', isSelected: false, value: 3.17527 },
+      { id: 'wq-city-1', isSelected: false, value: 3.410843 },
+      { id: 'wq-city-2', isSelected: false, value: 3.207926 },
+      { id: 'wq-city-3', isSelected: false, value: 2.98383 },
+      { id: 'wq-city-4', isSelected: false, value: 2.910456 },
+      { id: 'wq-city-5', isSelected: false, value: 3.248641 },
+      { id: 'wq-city-6', isSelected: false, value: 2.962557 },
+      { id: 'wq-city-7', isSelected: false, value: 3.346416 },
+      { id: 'wq-city-8', isSelected: false, value: 3.130143 },
+      { id: 'wq-city-9', isSelected: false, value: 3.350959 },
+    ]
   }
 ]
 
