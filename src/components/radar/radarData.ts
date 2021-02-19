b4e7ff37396deab0d3a93435614ccfdab33a5e94
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-02-19 15:58:10 +0200

    Update radar test data

--- src/components/radar/radarData.ts
+++ src/components/radar/radarData.ts
@@ -1,5 +1,23 @@
 export const characterData = [
-  { strength: 1, intelligence: 250, luck: 1, stealth: 40, charisma: 50 },
-  { strength: 2, intelligence: 300, luck: 2, stealth: 80, charisma: 90 },
-  { strength: 5, intelligence: 225, luck: 3, stealth: 60, charisma: 120 },
+  {
+    wq_fishery: 6.042483,
+    wq_city: 3.17527,
+    roi: 6.090291,
+    city_tax: 2.444406,
+    plant_resources: 0.248895,
+  },
+  {
+    wq_fishery: 5.758127,
+    wq_city: 3.410843,
+    roi: 6.887735,
+    city_tax: 8.989781,
+    plant_resources: 0.346752,
+  },
+  {
+    wq_fishery: 6.287081,
+    wq_city: 3.207926,
+    roi: 2.992514,
+    city_tax: 2.758216,
+    plant_resources: 0.326688,
+  },
 ]
