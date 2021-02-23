ec811f4f753b591bfdb4974a948a2932222a5087
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-02-23 19:30:28 +0200

    Add event handler types to parallel axes

--- src/types/eventHandlerTypes.ts
+++ src/types/eventHandlerTypes.ts
@@ -1,15 +1,8 @@
-import { AttributeSet, Coordinate, Value } from './dataTypes'
+import { Coordinate, Value } from './dataTypes'
 
-type OnChangeHandler = (active: AttributeSet[]) => void
 type OnClickHandler = (clicked: Value) => void
 type OnCoordinateClick = (clicked: Coordinate) => void
 type OnCoordinateSelect = (selected: Coordinate[]) => void
 type OnSelectionClearedHandler = () => void
 
-export type {
-  OnChangeHandler,
-  OnClickHandler,
-  OnCoordinateClick,
-  OnCoordinateSelect,
-  OnSelectionClearedHandler,
-}
+export type { OnClickHandler, OnCoordinateClick, OnCoordinateSelect, OnSelectionClearedHandler }
