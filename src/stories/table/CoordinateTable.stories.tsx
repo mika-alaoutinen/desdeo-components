8d9bc9a34b9d03eca1e615bec53ced9631de3c26
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-02-24 19:46:54 +0200

    Update CoordinateTable story

--- src/stories/table/CoordinateTable.stories.tsx
+++ src/stories/table/CoordinateTable.stories.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import { Meta } from '@storybook/react'
+import { Meta, Story } from '@storybook/react'
 
 import CoordinateTable from '../../components/table/coordinateTable/CoordinateTable'
 import { useCoordinateClickHandler } from '../hooks/coordinateChartHooks'
+import { coordinates } from '../../data/testdata'
+import { CoordinateChart } from '../../types/chartTypes'
 
 export const CoordinateTableWithHooks = (): JSX.Element => {
   const { data, onClick } = useCoordinateClickHandler()
@@ -10,7 +12,22 @@ export const CoordinateTableWithHooks = (): JSX.Element => {
   return <CoordinateTable data={data} onClick={onClick} />
 }
 
+const Template: Story<CoordinateChart> = args => <CoordinateTable {...args} />
+
+export const CoordinateChartTemplate = Template.bind({})
+CoordinateChartTemplate.args = {
+  data: coordinates,
+  onClick: coordinate => console.log('coordinate', coordinate),
+}
+
 export default {
   title: 'DESDEO/CoordinateTable',
   component: CoordinateTable,
+  argTypes: {
+    onClick: {
+      table: {
+        disable: true,
+      },
+    },
+  },
 } as Meta
