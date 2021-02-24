900d8e7821d8e90d75e2cafcdf3cdffd865fed5f
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-02-24 17:45:52 +0200

    Add barChartHooks to enable selections in stories

--- src/stories/bar/GroupedBarChart.stories.tsx
+++ src/stories/bar/GroupedBarChart.stories.tsx
@@ -2,13 +2,27 @@ import React from 'react'
 import { Meta, Story } from '@storybook/react'
 
 import { argTypes, props } from './storyConfig'
+import { useOnClickHandler } from './barChartHooks'
 import GroupedBarChartWrapper from '../../components/bar/grouped/GroupedBarChartWrapper'
 import { BarChartWrapperProps } from '../../types/chartTypes'
 
+export const GroupedBar = (): JSX.Element => {
+  const { data, grouping, onClick, orientation } = useOnClickHandler()
+
+  return (
+    <GroupedBarChartWrapper
+      data={data}
+      grouping={grouping}
+      onClick={onClick}
+      orientation={orientation}
+    />
+  )
+}
+
 const Template: Story<BarChartWrapperProps> = args => <GroupedBarChartWrapper {...args} />
 
-export const GroupedBarChart = Template.bind({})
-GroupedBarChart.args = props
+export const GroupedBarChartTemplate = Template.bind({})
+GroupedBarChartTemplate.args = props
 
 export default {
   title: 'DESDEO/GroupedBarChart',
